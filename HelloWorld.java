2da9f852fb3413e958e1b1d0d90dde38a394fdab
Revati11 <99247781+Revati11@users.noreply.github.com>
2024-08-30 12:48:55 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+static void method(){
+System.out.println("Hello Java Method");
+  System.out.println("Hello Change Java Method");
+}
+public static void main(String[] args){
+System.out.println("Hello Java Method");
+method();
+}
+}
+
